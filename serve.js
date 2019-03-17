c63df23d913610191a669a419b44ffdcae60209d
Aníbal Henrique <annibalhsouza@gmail.com>
2019-03-17 02:00:54 -0300

    #ISSUE Adding route to get message text client Facebook Messeger

--- serve.js
+++ serve.js
@@ -1,11 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const verification = require('./controllers/verification')
-const app = express()
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  verification = require('./controllers/verification'),
+  messageWebhook = require('./controllers/messageWebhook'),
+  app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', verification)
-
 app.listen(3000, () => console.log('Webhook server is listening, port 3000'))
+
+app.get('/', verification)
+app.post('/', messageWebhook)
